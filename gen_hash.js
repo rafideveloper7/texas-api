176d2b38199fd50b0c94848dfed77a5831401a4c
rafideveloper7 <rafideveloper7@gmail.com>
2026-03-31 10:27:53 +0500

    updates

--- gen_hash.js
+++ gen_hash.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const password = 'Admin@123';
+const password = process.env.ADMIN_PASSWORD;
 const salt = bcrypt.genSaltSync(10);
 const hash = bcrypt.hashSync(password, salt);
 require('fs').writeFileSync('hash_out.txt', hash);
